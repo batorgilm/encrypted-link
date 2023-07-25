b390e1f823764084596e01cb31521ba77b676b42
Batorgil <orgil@Batorgils-MacBook-Air.local>
2023-07-25 16:52:04 +0800

    a

--- src/pages/api/teams.ts
+++ src/pages/api/teams.ts
@@ -1,7 +1,9 @@
 import { ClientSecretCredential } from "@azure/identity";
 import { Client } from "@microsoft/microsoft-graph-client";
 import { TokenCredentialAuthenticationProvider } from "@microsoft/microsoft-graph-client/authProviders/azureTokenCredentials";
-export const sendChat = async () => {
+import axios from "axios";
+
+export default async function sendChat(req: any, res: any) {
   const CLIENT_ID = "404659cf-07ec-4947-bd24-58afa86de474";
   const CLIENT_SECRET = "5Ws8Q~nh4MsFub4gRVYxQf0_IMPIHwdEMNLR6cos";
   const TENANT_ID = "aae8fbe2-488b-49d9-b471-e4be61674a71";
@@ -48,15 +50,15 @@ export const sendChat = async () => {
     // console.log(graphClient);
 
     const response = await graphClient.api("/chats").post(chat);
-    // console.log("aaa", response);
+    console.log("aaa", response);
     const chatResponse = await graphClient
       .api(
         `/chats/19:46af2548-1d7a-4851-9467-1967911b4acd_ad8f25ec-0b16-478e-9e25-dfe283c413f5@unq.gbl.spaces/messages`
       )
       .post(msg);
 
-    console.log(chatResponse);
+    res.send({ response, chatResponse });
   } catch (error) {
-    console.log(error);
+    res.send({ error });
   }
-};
+}
